9d7737abd952429993e695a7db07a7b6692ae894
jyoun009 <jyoun009@gmail.com>
2015-05-05 21:54:57 -0700

    myflickr

---
+++ myflickr/app/src/main/java/com/gorgonshank/myflickr/Constants.java
@@ -0,0 +1,24 @@
+package com.gorgonshank.myflickr;
+
+/**
+ * Created by mark on 4/29/15.
+ */
+public class Constants {
+    public static final int NUM_PHOTOS = 10;
+    public static final String TAG = "my code";
+
+    public static final String API_KEY = "3e056d16425e7ebc02fb874c4b78f494";
+   // public static final String API_KEY = "8fd9185a9077637f9b6375d3e1394c7c";
+    public static final String SECRET = "a784c04d37352591";
+    //public static final String SECRET = "b1c8f0151a6c428c";
+
+//    public static final String LATITUDE = "34.0672280";
+//    public static final String LONGITUDE = "-118.1667410";
+    public static final String RADIUS = "1";
+
+    public static final String LATITUDE = "40.7127837";
+    public static final String LONGITUDE = "-74.00594130000002";
+
+
+
+}
